33937b56f33f8d97973af1b4a2d252bc9fd8ba09
xamgore <xamgore@ya.ru>
2017-07-21 20:25:04 +0300

    Search repositories by author

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,61 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div id="app" style="padding: 7em">
+    <!-- <router-view></router-view> -->
+
+    <input type="text" class="search-author" placeholder="nickname…" @input="setAuthor">
+
+    <repositories :repos="repos"/>
+
   </div>
 </template>
 
 <script>
+import repositories from '@/components/repositories';
+import debounce from '@/util/debounce';
+
 export default {
   name: 'app',
+  components: { repositories },
+  data: () => ({
+    author: '',
+  }),
+  methods: {
+    setAuthor: debounce(function (e) {
+      this.author = e.target.value;
+    }, 500),
+  },
+  asyncComputed: {
+
+    repos: {
+      default: [],
+      get() {
+        if (!this.author) return null;
+        const author = encodeURI(this.author);
+        const url = `https://api.github.com/users/${author}/repos?sort=pushed`;
+        return this.axios.get(url).then(res => res.data);
+      },
+    },
+
+  },
 };
 </script>
 
 <style>
+  * { box-sizing: inherit; }
 
+  html, body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  .search-author {
+    width: 100%;
+    margin: 0 0 2em;
+    padding: 0.3em;
+    line-height: 1.5em;
+    font-size: 1.2em;
+    font-family: 'Roboto Mono'
+  }
 </style>
---
+++ src/components/repo.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="repo" @click="goto(rep.name)">
+
+    <div class="repo__title">
+      <span class="repo__name">
+        {{ rep.name }}
+
+        <span v-if="rep.stargazers_count" class="repo__stars">
+          ⭐{{ rep.stargazers_count }}
+        </span>
+
+        <span v-if="rep.watchers_count" class="repo__watchers">
+          👀{{ rep.watchers_count }}
+        </span>
+      </span>
+    </div>
+
+
+    <div class="repo__desc">
+      {{ rep.description }}
+    </div>
+
+    <div class="repo__other" style="margin-top: 0.5em">
+      <span v-if="rep.language" class="repo__lang">{{ rep.language }}</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'repo',
+  props: ['rep'],
+  methods: {
+    goto(rep) { console.log(rep); },
+  },
+};
+</script>
+
+<style>
+  .repo {
+    padding: 1em;
+    border: 1px solid #ddd;
+    border-radius: 0.2em;
+    line-height: 1.5em;
+  }
+
+  .repo:hover {
+    background-color: #f6f6f6;
+    cursor: pointer;
+  }
+
+  .repo__name {
+    font-family: 'Roboto Mono', monospace;
+    font-weight: 500;
+    /*color: #1976d2;*/
+    color: #5C6BC0;
+  }
+
+  .repo__stars, .repo__watchers {
+    color: #555;
+  }
+
+  .repo__desc {
+    color: gray;
+  }
+
+  .repo__lang {
+    color: #9e9e9e;
+    font-size: 0.9em;
+  }
+</style>
