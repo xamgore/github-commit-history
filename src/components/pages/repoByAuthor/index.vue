13c0530899180ea5ecca47f7919e26d770f98694
xamgore <xamgore@ya.ru>
2017-07-22 13:07:19 +0300

    Add infinite scroll

--- src/components/pages/repoByAuthor/index.vue
+++ src/components/pages/repoByAuthor/index.vue
@@ -6,42 +6,59 @@
 
     <repositories :repos="repos" :err="error"/>
 
+    <infinite-loading v-show="author" :on-infinite="fetchMoreRepos" ref="infiniteLoading">
+      <span slot="no-more"/>
+      <span slot="no-results"/>
+    </infinite-loading>
+
   </div>
 </template>
 
 <script>
-import debounce from '@/util/debounce';
+import infiniteLoading from 'vue-infinite-loading';
+import { debounce } from '@/util/debounce';
 import repositories from './repositories';
 
 export default {
   name: 'page-by-author',
-  components: { repositories },
+  components: { repositories, infiniteLoading },
   data: () => ({
     author: 'github',
     error: '',
+    page: 0,
+    repos: null,
   }),
   methods: {
     setAuthor: debounce(function (e) {
       this.author = e.target.value;
+
+      // fetchMoreRepos will be called automatically
+      this.page = 0;
+      this.repos = null;
+      this.$refs.infiniteLoading.$emit('$InfiniteLoading:reset');
     }, 500),
-  },
-  asyncComputed: {
+    fetchMoreRepos: debounce(function () {
+      this.error = '';
+      if (!this.author.length) return;
 
-    repos: {
-      default: null,
-      get() {
-        this.error = '';
-        if (!this.author) return null;
-        const author = encodeURI(this.author);
-        const url = `https://api.github.com/users/${author}/repos?sort=pushed&client_id=07e0ab8ddd5a2a83cc80&client_secret=c78ac83e552cf858af63a8c8cde812a2a778fd7e`;
-        return this.axios.get(url).then(res => res.data)
-          .catch((err) => {
-            this.error = err.response && err.response.status === 404
-              ? 'User is not found' : 'Cannot connect to server';
-          });
-      },
-    },
+      this.page += 1;
+      const author = encodeURI(this.author);
+      const clientId = 'client_id=07e0ab8ddd5a2a83cc80';
+      const clientSecret = 'client_secret=c78ac83e552cf858af63a8c8cde812a2a778fd7e';
+      const url = `https://api.github.com/users/${author}/repos?page=${this.page}&sort=pushed&${clientId}&${clientSecret}`;
 
+      this.axios.get(url)
+        .then((res) => {
+          this.repos = (this.repos || []).concat(res.data);
+          const status = (res.data.length && 'loaded') || 'complete';
+          this.$refs.infiniteLoading.$emit(`$InfiniteLoading:${status}`);
+        })
+        .catch((err) => {
+          this.error = err.response && err.response.status === 404
+            ? 'User is not found' : 'Cannot connect to server';
+          this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete');
+        });
+    }, 1000, true),
   },
 };
 </script>
@@ -55,7 +72,7 @@ export default {
   .search-author {
     width: 100%;
     max-width: 440px;
-    margin: 1em auto 2em;
+    margin: 2em auto 2em;
     padding: 0.3em;
     box-sizing: content-box;
     line-height: 1.5em;
--- src/util/debounce.js
+++ src/util/debounce.js
@@ -1,8 +1,10 @@
+/* eslint-disable */
+
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
-export default function (func, wait, immediate) {
+const debounce = function (func, wait, immediate) {
   let timeout;
   return function (...args) {
     const context = this;
@@ -16,3 +18,42 @@ export default function (func, wait, immediate) {
     if (callNow) func.apply(context, args);
   };
 }
+
+// Returns a function, that, when invoked, will only be triggered at most once
+// during a given window of time. Normally, the throttled function will run
+// as much as it can, without ever going more than once per `wait` duration;
+// but if you'd like to disable the execution on the leading edge, pass
+// `{leading: false}`. To disable execution on the trailing edge, ditto.
+const throttle = function (func, wait, options = {}) {
+  let context;
+  let args;
+  let result;
+  let timeout = null;
+  let previous = 0;
+  const later = function () {
+    previous = options.leading === false ? 0 : Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function (...args) {
+    const now = Date.now();
+    if (!previous && options.leading === false) previous = now;
+    const remaining = wait - (now - previous);
+    context = this;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+}
+
+export { debounce, throttle }
