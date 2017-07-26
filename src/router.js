262046abb0b25d15a6dc4dcba5089d587b90aedf
xamgore <xamgore@ya.ru>
2017-07-26 11:27:43 +0300

    Change history mode to history
    Urls will not contain the hash symbol anymore.

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import repoByAuthor from '@/components/pages/profile';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     { path: '/:author?', name: 'byAuthor', props: true, component: repoByAuthor },
     { path: '/:author/:repo/:branch?', name: 'repo', props: true, component: repoPage },
